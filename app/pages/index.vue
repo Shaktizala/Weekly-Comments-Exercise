5b3dd3cf58a6537ec7466139d5da6e02a3ab26ea
monikagorkhiya <monika@improwised.com>
2023-04-09 03:01:02 +0530

    fix: index page changes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useUsersStore } from '~/store/users'
 const UsersStore = useUsersStore();
-console.log(UsersStore);
+//console.log(UsersStore);
 </script>
 <template>
   <div class="text-center homepage">Welcome to nuxt3</div>
