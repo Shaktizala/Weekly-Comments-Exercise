54844f44acf3ea2d871062cf525e63aa89590e99
monikagorkhiya <monika@improwised.com>
2023-04-09 02:54:56 +0530

    feat: add configuration for overall changes

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,27 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      base_url: process.env.BASE_URL,
+    },
+  },
+
   app: {
-
     head: {
-
-      title: "Nuxt3",
+      title: 'Nuxt3',
       meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
       script: [
         //This is just for example how to add js
         //you can  include js  by this method direact include or via import individual method as per below link
         //https://github.com/Debonex/samples/blob/master/nuxt3-bootstrap5/app.vue
-        { src: 'https://awesome-lib.js' }
+        { src: 'https://awesome-lib.js' },
       ],
       link: [
         //This for just example how to add css
-        { rel: 'stylesheet', href: 'https://awesome-lib.css' }
+        { rel: 'stylesheet', href: 'https://awesome-lib.css' },
       ],
-
-
-
     },
   },
 
