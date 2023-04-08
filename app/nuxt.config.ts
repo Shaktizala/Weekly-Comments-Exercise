b9cab14002b0e431f6c9c301e8f94913030c9f8c
monikagorkhiya <monika@improwised.com>
2023-04-09 01:35:41 +0530

    feat: add configuration

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+
+    head: {
+
+      title: "Nuxt3",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      script: [
+        //This is just for example how to add js
+        //you can  include js  by this method direact include or via import individual method as per below link
+        //https://github.com/Debonex/samples/blob/master/nuxt3-bootstrap5/app.vue
+        { src: 'https://awesome-lib.js' }
+      ],
+      link: [
+        //This for just example how to add css
+        { rel: 'stylesheet', href: 'https://awesome-lib.css' }
+      ],
+
+
+
+    },
+  },
+
+  css: ['bootstrap/dist/css/bootstrap.min.css'], // add
   modules: [
     [
       '@pinia/nuxt',
@@ -8,7 +34,7 @@ export default defineNuxtConfig({
       },
     ],
   ],
-  css: ['bootstrap/dist/css/bootstrap.min.css'], // add
+
   vite: {
     define: {
       'process.env.DEBUG': false,
