00d913d0a0e01d170d277735d7bee3eeeb00e5d0
Shaktiraj <shaktirajsinh.zala@improwised.com>
2023-04-19 17:22:07 +0530

    exercise done

---
+++ app/components/Comment.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <span>{{ comment }}</span>
+
+    <ul v-if="replies.length">
+      <div v-for="(item, index) in replies" :key="index">
+        <Comment
+          v-bind="{
+            comment: item.auther,
+            replies: item.comments,
+          }"
+        />
+      </div>
+    </ul>
+  </li>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  comment: {
+    type: String,
+    required: true,
+  },
+  replies: {
+    type: Array,
+    default: () => [],
+  },
+})
+</script>
